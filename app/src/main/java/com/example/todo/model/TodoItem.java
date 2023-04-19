bad90fbc7278efd05ba9a145ebdc1e3fc48d94a3
TimothyBorghouts <timothy.borghouts2@gmail.com>
2023-04-19 14:33:28 +0200

    Created ToDoItem class with the attributes: title, description, priority, due_date, notes

---
+++ app/src/main/java/com/example/todo/model/TodoItem.java
@@ -0,0 +1,57 @@
+package com.example.todo.model;
+
+public class TodoItem {
+    private String title;
+    private String description;
+    private int priority;
+    private String dueDate;
+    private String notes;
+
+    public TodoItem(String title, String description, int priority, String dueDate, String notes) {
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+        this.dueDate = dueDate;
+        this.notes = notes;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public String getDueDate() {
+        return dueDate;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public void setDueDate(String dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+}
